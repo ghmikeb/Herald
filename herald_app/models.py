32ff2b616a489388d27cc0458420527f847036ac
Mike Brannigan <mbrannigan@grubhub.com>
2013-12-19 16:57:36 -0600

    Initial model for incident

--- herald_app/models.py
+++ herald_app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class Incident(models.Model):
+
+    #
+    # Severity choices
+    #
+    SEV1 = 'S1'
+    SEV2 = 'S2'
+    SEV3 = 'S3'
+    SEV4 = 'S4'
+    SEVERITY_CHOICES = (
+        (SEV1, 'Severity 1'),
+        (SEV2, 'Severity 2'),
+        (SEV3, 'Severity 3'),
+        (SEV4, 'Maintenance'),
+    )
+    severity = models.CharField(max_length=2,
+                                choices=SEVERITY_CHOICES,
+                                default=SEV4
+    )
+
+    #
+    # Status Choices
+    #
+    STAT1 = 'S1'
+    STAT2 = 'S2'
+    STAT3 = 'S3'
+    STAT4 = 'S4'
+    STATUS_CHOICES = (
+        (STAT1, 'One-Time'),
+        (STAT2, 'Initial'),
+        (STAT3, 'Update'),
+        (STAT4, 'Final'),
+    )
+    status = models.CharField(max_length=2,
+                                choices=STATUS_CHOICES,
+                                default=STAT3
+    )
+
+    ticket   = models.CharField(max_length=20, blank=False, null=False)
+    subject  = models.CharField(max_length=20, blank=False, null=False)
+    description = models.TextField(max_length=120)
+    incident_start = models.DateTimeField(verbose_name='Incident Start', blank=False, null=False)
+    incident_end = models.DateTimeField(verbose_name='Incident End', null=True)
+    impact   = models.CharField(max_length=120)
+    next_update = models.DateTimeField(verbose_name='Next Update Time')
+
+    # timestamps
+    created_at = models.DateTimeField(auto_created=True, auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Log(models.Model):
+    incident = models.ForeignKey(Incident)
+    change_text = models.TextField(max_length=120, default="Initial entry")
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now=True, auto_created=True)
--- herald_app/tests.py
+++ herald_app/tests.py
@@ -1,7 +1,122 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.db import IntegrityError
+import datetime
 
-class HeraldAppTests(TestCase):
-    def test_simple(self):
-        self.assertEqual(1, 1)
+from herald_app.models import Incident, Log
+
+class HeraldAppModelTests(TestCase):
+
+    #
+    # Incident Test
+    #
+    def test_can_create_incident_and_retrieve_it(self):
+        new_incident = Incident.objects.create(
+            severity = 'S1',
+            status   = 'S1',
+            ticket   = 'GO-1234',
+            subject  = 'Porkchop sammitches',
+            description = 'My hair is on fire',
+            incident_start    = datetime.datetime(2013, 11, 01, 22, 00, 00),
+            impact   = 'All the things are down',
+            next_update =  datetime.datetime(2013, 11, 01, 22, 15, 00)
+        )
+
+        incident = Incident.objects.all()[0]
+
+        self.assertEqual(new_incident, incident)
+
+    def test_create_log_entry_when_new_incident_is_created(self):
+        new_incident = Incident.objects.create(
+            severity = 'S1',
+            status   = 'S1',
+            ticket   = 'GO-1234',
+            subject  = 'Porkchop sammitches',
+            description = 'My hair is on fire',
+            incident_start    = datetime.datetime(2013, 11, 01, 22, 00, 00),
+            impact   = 'All the things are down',
+            next_update =  datetime.datetime(2013, 11, 01, 22, 15, 00)
+        )
+        now = datetime.datetime.now()
+
+        new_log = Log.objects.create(
+            incident = new_incident
+        )
+
+        self.assertEqual(new_log.change_text, 'Initial entry')
+        self.assertEqual(new_log.incident.ticket, 'GO-1234')
+
+    def test_create_log_entry_will_create_a_created_at_timestamp(self):
+        new_incident = Incident.objects.create(
+            severity = 'S1',
+            status   = 'S1',
+            ticket   = 'GO-1234',
+            subject  = 'Porkchop sammitches',
+            description = 'My hair is on fire',
+            incident_start    = datetime.datetime(2013, 11, 01, 22, 00, 00),
+            impact   = 'All the things are down',
+            next_update =  datetime.datetime(2013, 11, 01, 22, 15, 00)
+        )
+        now = datetime.datetime.now()
+
+        new_log = Log.objects.create(
+            incident = new_incident
+        )
+
+        self.assertEqual(new_log.change_text, 'Initial entry')
+        self.assertEqual(new_log.incident.ticket, 'GO-1234')
+        created_at = new_log.incident.created_at
+        self.assertEqual(
+            ( created_at.year, created_at.month, created_at.day,
+              created_at.hour, created_at.minute ),
+            ( now.year, now.month, now.day, now.hour, now.minute )
+        )
+
+    def test_modifying_log_entry_will_update_a_modified_at_timestamp(self):
+        incident = Incident.objects.create(
+            severity = 'S1',
+            status   = 'S1',
+            ticket   = 'GO-1234',
+            subject  = 'Porkchop sammitches',
+            description = 'My hair is on fire',
+            incident_start    = datetime.datetime(2013, 11, 01, 22, 00, 00),
+            impact   = 'All the things are down',
+            next_update =  datetime.datetime(2013, 11, 01, 22, 15, 00)
+        )
+
+        new_log = Log.objects.create(
+            incident = incident
+        )
+
+        self.assertEqual(new_log.change_text, 'Initial entry')
+        self.assertEqual(new_log.incident.ticket, 'GO-1234')
+        created_at = new_log.incident.created_at
+
+        # wait a minute
+        print "Waiting a minute"
+        import time
+        time.sleep(60)
+
+        # Modify the incident
+        incident.description = "It is less of an issue now"
+        incident.severity = 'S2'
+        incident.save()
+        incident_update = incident.modified_at
+
+        new_log = Log.objects.create(
+            incident = incident,
+            change_text = "Decreased severity"
+        )
+
+        self.assertEqual(new_log.incident.description, "It is less of an issue now")
+        self.assertEqual(new_log.incident.severity, 'S2')
+        self.assertNotEqual(
+            (incident_update.year, incident_update.month, incident_update.day,
+             incident_update.hour, incident_update.minute),
+            ( created_at.year, created_at.month, created_at.day,
+              created_at.hour, created_at.minute )
+        )
+
+
+
+
